75426a38942d6d42f538274e8e09fae6ff3b3129
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-10 20:54:45 +0200

    Connected routes, proccessing json data is able

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import connectDB from "./db.js";
+import postRouter from "./routes/posts.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 connectDB();
+app.use(express.json());
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server");
