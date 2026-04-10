d79e6897c3c4fe29c440872c3cf83b341e6621ee
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-10 20:37:40 +0200

    imported connectDB to connect mongo db to server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import connectDB from "./db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server");
