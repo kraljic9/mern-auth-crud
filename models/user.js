e995436276439a1655df98ae7e1b005456d3ce97
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-12 22:30:28 +0200

    Added file to create userSchema

---
+++ models/user.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("password")) return next();
+    const salt = bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
