7f661668dbe4275a400ae56f678d7f7a85df6dc7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-12 23:08:16 +0200

    Added login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
 import User from "../models/user.js";
-
+import bcrypt from "bcrypt";
 const router = express.Router();
 
 // Register user
@@ -26,11 +26,43 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "Error creating user failed" });
     }
 
+    const isMatch = await User.findOne({ email });
+
+    if (isMatch) {
+      return res.status(400).json({ message: "Error user already exists" });
+    }
+
     res.status(201).json({ message: `User created, welcome ${username}` });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Server error" });
   }
+});
+
+// Login user
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Error all fields must be filled" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: "Error user not found" });
+  }
+
+  console.log(user, user.password);
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Error wrong password input" });
+  }
+
+  res.status(201).json({ message: `User logged in, welcome ${user.username}` });
 });
 
 export default router;
