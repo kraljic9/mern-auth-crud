0fb12635656e2affa117565de19e2af23253bd76
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-12 22:48:09 +0200

    Connected route

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import connectDB from "./db.js";
 import postRouter from "./routes/posts.js";
-
+import userRouter from "./routes/auth.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
 app.use("/posts", postRouter);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server");
