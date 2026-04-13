11336d7033c51169710eff6da0e2eced37a22b0b
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-04-13 16:16:56 +0200

    moved existingUser variable

--- routes/auth.js
+++ routes/auth.js
@@ -16,27 +16,22 @@ router.post("/register", async (req, res) => {
         .json({ message: "Error all fields must be filled" });
     }
 
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Error user already exists" });
+    }
     const user = await User.create({
       username,
       email,
       password,
     });
 
-    if (!user) {
-      return res.status(400).json({ message: "Error creating user failed" });
-    }
-
-    const isMatch = await User.findOne({ email });
-
-    if (isMatch) {
-      return res.status(400).json({ message: "Error user already exists" });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
 
-    res.status(201).json({ user: { id: user.id, username, email }, token });
+    res.status(201).json({ user: { id: user._id, username, email }, token });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Server error" });
@@ -46,29 +41,38 @@ router.post("/register", async (req, res) => {
 // Login user
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ message: "Error all fields must be filled" });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Error all fields must be filled" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Error user not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Error wrong password input" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res
+      .status(200)
+      .json({ user: { id: user._id, username: user.username, email }, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
   }
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return res.status(400).json({ message: "Error user not found" });
-  }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
-    return res.status(400).json({ message: "Error wrong password input" });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.status(200).json({ user: { id: user.id, username, email }, token });
 });
 
 export default router;
