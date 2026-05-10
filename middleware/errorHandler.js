b6a1b32e665aaf25ee9ac7ad9147d30c7afb96d0
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-10 20:28:02 +0200

    Added if statement to check type of error message

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,14 @@
 export const errorHandler = (err, res, req, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const message = err.message;
+
+  if (err.message === "ZodError") {
+    statusCode = 400;
+    message = err.errors.map((e) => e.message).join(", ");
+  }
 
   res.status(statusCode).json({
-    message: err.message,
+    message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
