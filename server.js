6a67733d3f34c8ced806add0be6f7df56679cb0c
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-11 15:01:00 +0200

    Imported comment router to use

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./db.js";
 import postRouter from "./routes/posts.js";
 import userRouter from "./routes/auth.js";
+import commentRouter from "./routes/comment.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
@@ -11,6 +12,7 @@ connectDB();
 app.use(express.json());
 app.use("/posts", postRouter);
 app.use("/user", userRouter);
+app.use("/comment", commentRouter);
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
