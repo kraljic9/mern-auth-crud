4b18aab342b8f216cc6846e32f8e504cd289acb3
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-11 14:56:07 +0200

    Renamed file

---
+++ models/comments.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: string,
+    required: true,
+  },
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Comment", commentSchema);
