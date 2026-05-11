689934e4582080a431cdb0121df99f2705764735
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-11 15:20:11 +0200

    Added route for createing comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,23 @@
 import express from "express";
+import auth from "../middleware/auth";
+import { asyncHandler } from "../utils/asyncHandler";
 
 const router = express.Router();
+
+router.post(
+  "/",
+  auth,
+  asyncHandler(async (req, res) => {
+    const { content, postId } = req.body;
+
+    const comment = await Comment.create({
+      content,
+      post: postId,
+      auth: req.user,
+    });
+
+    res.status(201).json(comment);
+  }),
+);
 
 export default router;
